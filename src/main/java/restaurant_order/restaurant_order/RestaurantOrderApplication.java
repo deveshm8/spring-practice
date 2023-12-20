32aebce69c94d1bbc0c77c2f1c96598c3a5e70f0
deveshm8 <deveshmiws@gmail.com>
2023-12-21 00:49:15 +0530

    initial commit

---
+++ src/main/java/restaurant_order/restaurant_order/RestaurantOrderApplication.java
@@ -0,0 +1,14 @@
+package restaurant_order.restaurant_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantOrderApplication.class, args);
+		System.out.print("hi all, this is devesh");
+	}
+
+}
---
+++ src/test/java/restaurant_order/restaurant_order/RestaurantOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package restaurant_order.restaurant_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
